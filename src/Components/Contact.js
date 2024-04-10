57be60ba39e01d407f69f5878f2875b61fb82218
Sam <samnewell91@gmail.com>
2024-04-10 14:44:10 +0100

    Added contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,36 @@
+import { Fade } from "react-awesome-reveal";
 export default function Contact() {
   return (
     <div className="page" id="Contact">
-      <h2>Contact</h2>
+      <Fade delay={300}>
+        <div className="contact-content">
+          <h1>Get in touch!</h1>
+          <p>
+            If you have any inquiries regarding my work, are interested in
+            potential collaborations, or simply wish to get in touch then please
+            don't hesitate to reach out, either by the form below or by
+            connecting with me through my social media channels.
+          </p>
+          <form>
+            <div className="form-top">
+              <input
+                id="Name"
+                name="name"
+                type="text"
+                placeholder="Name?"
+              ></input>
+              <input
+                id="Email"
+                name="email"
+                type="email"
+                placeholder="Email?"
+              ></input>
+            </div>
+            <textarea id="Message" name="message"></textarea>
+            <button>Submit</button>
+          </form>
+        </div>
+      </Fade>
     </div>
   );
 }
