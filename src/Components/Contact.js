e40ca93e125a7324a9bb4f54a126c4b9eb9049db
Sam <samnewell91@gmail.com>
2024-04-10 15:08:39 +0100

    trying to get the form to submit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
             don't hesitate to reach out, either by the form below or by
             connecting with me through my social media channels.
           </p>
-          <form name="contact" netlify>
+          <form name="contact" method="POST" netlify>
             <input type="hidden" name="form-name" value="contact"></input>
             <div className="form-top">
               <input
